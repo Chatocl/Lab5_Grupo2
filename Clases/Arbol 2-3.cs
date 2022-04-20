cd5b70129a633a38c495b4d93affd6c2cff9ea50
CARLOS ROBERTO CORONADO LAZO <crcoronado@correo.url.edu.gt>
2022-04-20 15:09:55 -0600

    Creación de clases

---
+++ Clases/Arbol 2-3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clases
+{
+    public class Arbol_2_3<T> : Nodo<T> where T : IComparable<T>
+    {
+        private Nodo<T> Raiz = new Nodo<T>();
+        private Nodo<T> temp = new Nodo<T>();
+        private T temp2;
+        public int ObtenerFE(Nodo<T> n)
+        {
+
+            if (n == null)
+            {
+                return -1;
+            }
+            else
+            {
+                return n.FE;
+            }
+
+        }
+
+        public void insert(T value, Nodo<T> nodo)
+        {
+            if (Raiz.Val1 != null || Raiz.Val2 != null)
+            {
+                if (Raiz.Val1 != null)
+                {
+                    Raiz.Val1 = value;
+                }
+                else if (Raiz.Val2 != null)
+                {
+                    if (Raiz.Val1.CompareTo(value) == -1) 
+                    {
+                        Raiz.Val2 = value;
+                    }
+                    else
+                    {
+                        temp2 = Raiz.Val1;
+                        Raiz.Val1 = value;
+                        Raiz.Val2 = temp2;
+                    }
+                   
+                }
+            }
+            else if (nodo. ) 
+            {
+                
+            }
+        }
+    }
+}
---
+++ Clases/Nodo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clases
+{
+    public class Nodo<T> where T : IComparable<T>
+    {
+        public Nodo<T> PriHijo { get; set; }
+        public Nodo<T> SegHijo { get; set; }
+        public Nodo<T> TerHijo { get; set; }
+        public Nodo<T> Padre { get; set; }
+        public T Val1 { get; set; }
+        public T Val2 { get; set; }
+        public int FE { get; set; }
+
+    }
+}
