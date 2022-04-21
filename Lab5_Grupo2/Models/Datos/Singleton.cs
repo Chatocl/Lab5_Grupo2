d3af11e4a31dac0b4872069c471d3061e4ebbad5
CARLOS ROBERTO CORONADO LAZO <crcoronado@correo.url.edu.gt>
2022-04-20 19:51:04 -0600

    Create y Controller Creado

---
+++ Lab5_Grupo2/Models/Datos/Singleton.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab5_Grupo2.Models.Datos
+{
+    public class Singleton
+    {
+        private static Singleton _instance = null;
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Singleton();
+                return _instance;
+            }
+        }
+        public Clases.Arbol_2_3<Vehiculos> ArbolVehiculos = new Clases.Arbol_2_3<Vehiculos>();
+    }
+}
---
+++ Lab5_Grupo2/Models/Vehiculos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Lab5_Grupo2.Models.Datos;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Lab5_Grupo2.Models
+{
+    public class Vehiculos: IComparable<Vehiculos>
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public int Placa { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Color { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Propietario { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public int Latitud { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public int Longitud { get; set; }
+
+        public int CompareTo(Vehiculos Otro)
+        {
+            if (Otro==null)
+            {
+                return 0;
+            }
+            else
+            {
+                return this.Placa.CompareTo(Otro.Placa);
+            }
+        }
+    }
+}
